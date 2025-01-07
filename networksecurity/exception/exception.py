22705fd996eccdeaf9591117e59eb1aa570ecd5a
Girish <girish.ikhe.vu@gmail.com>
2025-01-07 12:16:56 +0530

    updated code

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,5 +1,5 @@
 import sys
-from networksecur
+from networksecurity.logging import logger
 
 class NetworkSecurityException(Exception):
     def __init__(self,error_message,error_details:sys):
@@ -13,6 +13,14 @@ class NetworkSecurityException(Exception):
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         self.file_name, self.lineno, str(self.error_message))
         
+if __name__=='__main__':
+    try:
+        logger.logging.info("Enter the try block")
+        a=1/0
+        print("This will not be printed",a)
+    except Exception as e:
+           raise NetworkSecurityException(e,sys)
+
 if __name__=='__main__':
     try:
         logger.logging.info("Enter the try block")
